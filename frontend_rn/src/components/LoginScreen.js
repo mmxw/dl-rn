50aa862785751738e5324165db65308e049c93c8
cara <xw33@outlook.com>
2020-05-26 13:00:21 +0100

    add login form

--- frontend_rn/src/components/LoginScreen.js
+++ frontend_rn/src/components/LoginScreen.js
@@ -1,12 +1,28 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+// Formik x React Native example
+import React from "react";
+
+import { Button, TextInput, Text, View } from "react-native";
+import { Formik } from "formik";
+import style from '../styles/appStyles'
+import styles from "../styles/appStyles";
+
 
 const LoginScreen = () => {
+  
   return (
-    <View>
-      <Text>Login</Text>
-    </View>
-  )
-}
+    <Formik
+      initialValues={{ username: "", password:"" }}
+      onSubmit={(values) => console.log(values)}
+    >
+      <View style={styles.form}>
+        <Text style={styles.header}>Username</Text>
+        <TextInput />
+        <Text style={styles.header}>Password</Text>
+        <TextInput />
+        <Button title="Submit" />
+      </View>
+    </Formik>
+  );
+};
 
-export default LoginScreen
+export default LoginScreen;
--- frontend_rn/src/components/RegisterScreen.js
+++ frontend_rn/src/components/RegisterScreen.js
@@ -12,7 +12,7 @@ export default function RegisterScreen() {
 
   return (
     <View style={styles.form}>
-      <Text>USERNAME</Text>
+      <Text style={styles.header}>Username</Text>
       <Controller
         as={TextInput}
         control={control}
@@ -20,51 +20,51 @@ export default function RegisterScreen() {
         onChange={args => args[0].nativeEvent.text}
         rules={{ required: true }}
         defaultValue=""
+        style={styles.textinput}
       />
       {errors.firstName && <Text>This is required.</Text>}
 
-      <Text>EMAIL</Text>
+      <Text style={styles.header}>Email</Text>
       <Controller
         as={TextInput}
         control={control}
         name="lastName"
         onChange={args => args[0].nativeEvent.text}
         defaultValue=""
+        style={styles.textinput}
       />
-      <Text>EMAIL</Text>
-      <Controller
-        as={TextInput}
-        control={control}
-        name="email"
-        onChange={args => args[0].nativeEvent.text}
-        defaultValue=""
-      />
-      <Text>PASSWORD</Text>
+
+      <Text style={styles.header}>Password</Text>
       <Controller
         as={TextInput}
         control={control}
         name="password"
         onChange={args => args[0].nativeEvent.text}
         defaultValue=""
+        style={styles.textinput}
+        secureTextEntry={true}
       />
-      <Text>PASSWORD CONFIRMATION</Text>
+      <Text style={styles.header}>Password Confirmation</Text>
       <Controller
         as={TextInput}
         control={control}
         name="passwordConfirmation"
         onChange={args => args[0].nativeEvent.text}
         defaultValue=""
+        style={styles.textinput}
+        secureTextEntry={true}
       />
-      <Text>GENDER</Text>
+      <Text style={styles.header}>Gender</Text>
       <Controller
         as={TextInput}
         control={control}
         name="GENDER"
         onChange={args => args[0].nativeEvent.text}
         defaultValue=""
+        style={styles.textinput}
       />
 
-      <Button title="Submit" onPress={handleSubmit(onSubmit)} />
+      <Button title="Submit" onPress={handleSubmit(onSubmit)} style={styles.btn}/>
     </View>
   );
 }
